79f7aa3ca03f24eadb3c9d6a1c4e78b1a0a9cb53
Ryan Davis <ryandavis@Ryans-MacBook-Pro.local>
2022-09-17 19:32:59 +0100

    ad

--- src/components/PostPageContent.js
+++ src/components/PostPageContent.js
@@ -1,40 +1,59 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useQuery, gql } from "@apollo/client";
 
 const formatDate = (date) => new Date(date).toLocaleDateString();
 
 export default function PostPageContent({ post }) {
   const { date, title, content, author, categories } = post;
-  const haveCategories = Boolean(categories?.nodes?.length);
+
+  const searchQuery = window.location.pathname.split('/')[2];
+
+  
+
+  const SINGLE_RESOURCE = gql`
+  {
+    resource(id: "${searchQuery}", idType: SLUG) {
+      id
+      databaseId
+      title
+      uri
+    }
+  }
+  `;
+  
+ 
+
+
+
+  const { data, loading, error } = useQuery(SINGLE_RESOURCE);
+
+  if (loading) return <p>Loading posts...</p>;
+  if (error) return <p>Error :(</p>;
+
+
+
+  console.log(data.resource.title);
 
   return (
     <article>
-      <h1>{title}</h1>
+      <h1>{data.resource.title}</h1>
       <p className="post-meta">
         <span role="img" aria-label="writing hand">
           ✍️
         </span>{" "}
-        {author.node.name} on {formatDate(date)}
+         on {formatDate(date)}
       </p>
       <div
         className="post-content"
         dangerouslySetInnerHTML={{ __html: content }}
       />
-      {haveCategories ? (
+      
         <div className="categories-list">
           <h2>Categorized As</h2>
-          <ul>
-            {categories.nodes.map((category) => {
-              const { slug, name } = category;
-              return (
-                <Link to={`/category/${slug}`} key={slug}>
-                  <li key={slug}>{name}</li>
-                </Link>
-              );
-            })}
-          </ul>
+          
         </div>
-      ) : null}
+      
     </article>
   );
 }
